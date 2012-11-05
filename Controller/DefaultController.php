cad55fb99c206c632187d8533c36e55f0bf0f235
ldandoy <ldandoy@overconsulting.net>
2012-11-05 13:15:14 +0100

    correction nom bundle

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace startpack\CoreBundle\Controller;
+namespace StartPack\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
--- CoreBundle.php
+++ CoreBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace startpack\CoreBundle;
+namespace StartPack\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
