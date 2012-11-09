7e2ff4e81d97c22a604768dff625daa2cf40a096
ldandoy <ldandoy@overconsulting.net>
2012-11-09 19:13:54 +0100

    test

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-namespace StartPack\FrontBundle\Controller;
-=======
 namespace StartPack\CoreBundle\Controller;
->>>>>>> 6d4e0cf813f45af83d5553f0a2067935f11eb189
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-namespace StartPack\FrontBundle\DependencyInjection;
-=======
 namespace StartPack\CoreBundle\DependencyInjection;
->>>>>>> 6d4e0cf813f45af83d5553f0a2067935f11eb189
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -22,11 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-<<<<<<< HEAD
-        $rootNode = $treeBuilder->root('front');
-=======
         $rootNode = $treeBuilder->root('core');
->>>>>>> 6d4e0cf813f45af83d5553f0a2067935f11eb189
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
