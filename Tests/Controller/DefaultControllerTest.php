ec52e0d873f861fb12321e2aa2f950d8d1b3ef86
ldandoy <ldandoy@overconsulting.net>
2012-11-12 11:04:38 +0100

    nettoyage

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-namespace StartPack\FrontBundle\Tests\Controller;
-=======
 namespace StartPack\CoreBundle\Tests\Controller;
->>>>>>> 6d4e0cf813f45af83d5553f0a2067935f11eb189
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
