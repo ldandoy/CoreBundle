caf79db4eeecc290f64ed508b814700c563f1031
ldandoy <ldandoy@overconsulting.net>
2012-11-22 13:44:00 +0100

    Core

---
+++ Form/PageType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace StartPack\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('content')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'StartPack\CoreBundle\Entity\Page'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'startpack_corebundle_pagetype';
+    }
+}
--- sql/startpack.sql
+++ sql/startpack.sql
@@ -68,6 +68,85 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `email`, `password`, `role`, `salt`, `created_at`, `updated_at`) VALUES
 (1, 'l@disko.fr', 'xtTYO6w8gcSM9j/iKTJ0Xj1iOtyjlEmskuyxuO+4K+e6ISZ6k1ffqq83OKQfMe9JLVXeNa/VBzuooaMpaTckvQ==', 'ROLE_USER', '6uun4qgswps0o4kw040w8sk4kcgoowc', 0, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `config`
+--
+
+CREATE TABLE IF NOT EXISTS `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(50) NOT NULL,
+  `value` text NOT NULL,
+  `label` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `config`
+--
+
+INSERT INTO `config` (`id`, `key`, `value`, `label`) VALUES
+(1, 'nom-site', 'StartPack', 'Nom du site');
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `page`
+--
+
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `page`
+--
+
+INSERT INTO `page` (`id`, `name`, `slug`, `content`) VALUES
+(1, 'Test', 'page-de-test', 'Content de la page');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_info`
+--
+
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `last_name` varchar(250) NOT NULL,
+  `first_name` varchar(250) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `cp` varchar(5) NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `optin` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `user_info`
+--
+
+INSERT INTO `user_info` (`id`, `user_id`, `last_name`, `first_name`, `address`, `cp`, `ville`, `optin`) VALUES
+(1, 1, 'Mathieu', 'MARCHOIS', '40 rue de vaux', '95620', 'Parmain', 1);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
